c740693fce0a39e41f6473d9043104d665c37402
obazeec <obazee.esosa@yahoo.com>
2026-01-22 13:32:03 +0000

    Robust Error Handling added

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
-app.get("/", baseController.buildHome);
+
+app.get("/", utilities.handleErrors(baseController.buildHome));
 // Inventory route
 app.use("/inv", inventoryRoute)
 app.use(async (req, res, next) =>{
@@ -42,9 +43,10 @@ app.use(async (req, res, next) =>{
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){message = err.message} else{message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
