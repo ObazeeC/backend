3581ff46684af5d5fc34ab0a442b4ee5c0676694
obazeec <obazee.esosa@yahoo.com>
2026-02-02 19:18:06 +0000

    management view

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -7,6 +7,6 @@ const invController = require("../controllers/invController")
 router.get("/type/:classificationId", invController.buildByClassificationId);
 router.get("/detail/:inv_id", invController.buildDetailView)
 
-router.get("/inv", invController.buildManagement)
+router.get("/", invController.buildManagement)
 
 module.exports = router;
